e829bce7f972b4571e5eee795decf0321225f9a8
Raju <Saravanakumar.Raju@emc.com>
2021-05-28 12:09:14 +0530

    added lab1

---
+++ 2.C/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Thank You, Goddess Sarswathi for helping me write my first C code and Thank You so much CS50/David Malan and Thank You universe for getting this course to me at my doorstep! \n");
+}
